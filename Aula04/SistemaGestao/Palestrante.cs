a99724eaab57da11e5695252135aac5771111b62
OThurDev <arthurbcg16@gmail.com>
2026-03-17 20:18:13 -0300

    carga inicial

---
+++ Aula04/SistemaGestao/Palestrante.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SistemaGestaoEventos;
+
+public class Palestrante
+{
+    public String Id {get;set;}
+
+    public String Nome { get; set; }
+
+    public String Telefone { get; set; }
+
+    public String Email { get; set; }
+
+    public decimal ValorHora { get; set; }
+}
